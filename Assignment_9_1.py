dd6e881a386b17cca10ad6812d59f333f70193e4
ravikhairnar2502-alt <ravi.khairnar2502@gmail.com>
2026-01-18 12:21:39 +0530

    commit

---
+++ Assignment_9_1.py
@@ -0,0 +1,18 @@
+
+def Display():
+    print("Jay Ganesh ...........")
+
+def main():
+    Display()
+
+if __name__ == "__main__":
+    main()
+
+
+
+# Output :
+
+# C:\Users\EveryThink\Documents\Marvellous\Assignment>python Assignment_9_1.py
+# Jay Ganesh ........... 
+
+
