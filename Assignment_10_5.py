fee65701fad27d711a248728bcc684a9388a0b71
ravikhairnar2502-alt <ravi.khairnar2502@gmail.com>
2026-01-19 21:55:02 +0530

    commit

---
+++ Assignment_10_5.py
@@ -0,0 +1,19 @@
+def main():
+
+    Value1 = int(input("Enter number : "))
+    Ans = 0
+
+    for i in range(1,Value1+1,2):
+        print(i, end=' ')
+
+    
+if __name__ == "__main__":
+    main()
+
+
+
+# Output :
+
+# C:\Users\EveryThink\Documents\Marvellous\Assignment>python Assignment_10_5.py
+# Enter number : 10
+# 1 3 5 7 9
