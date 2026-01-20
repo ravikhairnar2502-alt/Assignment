3906a94ec3d37d7fd2b524b5d6ad1c0c6a83965a
ravikhairnar2502-alt <ravi.khairnar2502@gmail.com>
2026-01-20 22:21:34 +0530

    Add files via upload

---
+++ Assignment_13/Assignment_13_3.py
@@ -0,0 +1,30 @@
+# 3. Write a program which accepts one number and checks whether it is perfect number or not.
+
+def main():
+
+    No1 = int(input("Enter Radius : "))
+    sum = 0
+
+    for i in range(1,No1):
+        if No1 % i == 0:
+            sum += i
+
+    if sum == No1:
+        print("Perfect Number")
+    else:
+        print("Non Perfect Number")
+
+if __name__ == "__main__":
+    main()
+
+
+
+# Output :
+
+# C:\Users\EveryThink\Documents\Marvellous\Assignment\Assignment_13>python Assignment_13_3.py
+# Enter Radius : 6
+# Perfect Number
+
+# C:\Users\EveryThink\Documents\Marvellous\Assignment\Assignment_13>python Assignment_13_3.py
+# Enter Radius : 7
+# Non Perfect Number
---
+++ Assignment_13/Assignment_13_4.py
@@ -0,0 +1,18 @@
+# 4. Write a program which accepts one number and prints binary equivalent.
+
+def main():
+
+    No1 = int(input("Enter Radius : "))
+    
+    print(bin(No1)[2:])
+
+if __name__ == "__main__":
+    main()
+
+
+
+# Output :
+
+# C:\Users\EveryThink\Documents\Marvellous\Assignment\Assignment_13>python Assignment_13_4.py
+# Enter Radius : 10
+# 1010
