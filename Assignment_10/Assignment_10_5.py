a57aa0d78ab8ad7078a7980f3120ab4413401bab
ravikhairnar2502-alt <ravi.khairnar2502@gmail.com>
2026-01-20 19:29:34 +0530

    Update Assignment_10_5.py

--- Assignment_10/Assignment_10_5.py
+++ Assignment_10/Assignment_10_5.py
@@ -1,3 +1,5 @@
+# 5.Write a program which accepts one number and prints all odd numbers till that number.
+
 def main():
 
     Value1 = int(input("Enter number : "))
@@ -16,4 +18,5 @@ if __name__ == "__main__":
 
 # C:\Users\EveryThink\Documents\Marvellous\Assignment>python Assignment_10_5.py
 # Enter number : 10
+
 # 1 3 5 7 9
