8456abfc6932bd66042b936f63a269acb26b30ce
ravikhairnar2502-alt <ravi.khairnar2502@gmail.com>
2026-01-20 19:32:27 +0530

    Update Assignment_11_3.py

--- Assignment_11/Assignment_11_3.py
+++ Assignment_11/Assignment_11_3.py
@@ -1,3 +1,5 @@
+# 3. Write a program which accepts one number and prints sum of digits.
+
 def main():
 
     Value1 = input("Enter number : ")
@@ -19,4 +21,5 @@ if __name__ == "__main__":
 
 # C:\Users\EveryThink\Documents\Marvellous\Assignment\Assignment_11>python Assignment_11_3.py
 # Enter number : 123
+
 # 6
